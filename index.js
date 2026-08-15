155dd5779eb8a177f0b649bcfdc949ba20f0267c
jboddo12-design <jboddo12@gmail.com>
2026-08-15 15:27:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let lastAircraft = new Set();
 
 async function getAircraft() {
   const url =
-    `https://api.adsb.one/v2/point/${KFLL_LAT}/${KFLL_LON}/${RADIUS_NM}`;
+    `https://api.adsb.lol/v2/point/${KFLL_LAT}/${KFLL_LON}/${RADIUS_NM}`;
 
   const response = await axios.get(url, {
     timeout: 15000
